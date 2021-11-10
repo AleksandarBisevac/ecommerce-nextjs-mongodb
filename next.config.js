818adf22f85f7c10214ac5b8834dabeafa1c6265
AleksandarBisevac <alek.bisevac@gmail.com>
2021-11-10 22:39:00 +0100

    cleanup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+const webpack = require('webpack');
+
 module.exports = {
-  env: {
-    BASE_URL: 'http//localhost:3000',
-    MONGODB_URL:
-      'mongodb+srv://beerduke:beerduke-1389@cluster0.spjnx.mongodb.net/ecommerce-practice?retryWrites=true&w=majority',
+  webpack: (config) => {
+    config.plugins.push(new webpack.EnvironmentPlugin(process.env));
+    return config;
   },
 };
